3f1bedd9e88ae96835bfa15298e68a00c6ce7e8a
MarcosHerrera95 <138625261+MarcosHerrera95@users.noreply.github.com>
2024-07-01 14:05:51 -0300

    database.py

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL de la base de datos SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./university_enrollment.db"
+DATABASE_URL = "sqlite:///UNAB_Inscripciones.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
